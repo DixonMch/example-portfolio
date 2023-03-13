0a024f3d495b0c941b759b54245ec89d2ea5afd1
DixonMch <dixonmogaka@gmail.com>
2023-03-13 23:38:34 +0300

    add script.js, connect to index.html

---
+++ portfolio/script.js
@@ -0,0 +1,6 @@
+function handleClick () {
+    alert("Thank you for checking my details.")
+}
+const h2 = document.createElement("h2");
+    h2.textContent = "This content added by Javascript";
+    document.querySelector("body").appendChild(h2);
